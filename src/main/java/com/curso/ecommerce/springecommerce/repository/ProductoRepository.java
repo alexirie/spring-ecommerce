95c976cb63b96f4b6443ed73eb10416c359f166c
alexirie <alex_gayan@hotmail.com>
2022-12-15 20:46:41 +0100

    crear repository, las clases servicio, controlador producto y método show

---
+++ src/main/java/com/curso/ecommerce/springecommerce/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.curso.ecommerce.springecommerce.repository;
+
+import com.curso.ecommerce.springecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
