0c671970f5be5b549b0fafde38c26e195f1b9ab9
alexirie <alex_gayan@hotmail.com>
2022-12-21 19:48:33 +0100

    Añadidos métodos create y save y logger

--- src/main/java/com/curso/ecommerce/springecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/ProductoController.java
@@ -1,17 +1,48 @@
 package com.curso.ecommerce.springecommerce.controller;
 
+import com.curso.ecommerce.springecommerce.model.Producto;
+import com.curso.ecommerce.springecommerce.model.Usuario;
+import com.curso.ecommerce.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+    private final Logger logger = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private ProductoService productoService;
+
+
     @GetMapping("")
     public String show(){
         return "productos/show";
     }
+
+    @GetMapping("/create")
+    public String create(){
+
+        return "productos/create";
+    }
+    @PostMapping("/save")
+    public String save(Producto producto){
+
+        logger.info("Este es el producto: {}", producto);
+
+        Usuario user = new Usuario(2,"","","","","","","");
+        producto.setUsuario(user);
+        productoService.save(producto);
+        return "redirect:/productos";
+    }
+
+
 
 
 }
